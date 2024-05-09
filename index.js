a46dd67b2c2c0d1622ce5dcb3d30b954aba37867
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-05-09 23:25:29 +0300

    Setup a server side for Auto Librarian, a LearnEdge e-Library

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Auto Librarian, a LearnEdge e-Library!");
+});
+
+app.listen(port, () => {
+  console.log(`Auto Librarian server running on port ${port}!`);
+});
